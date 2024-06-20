e770a7e2d5fe6b76b3e7b644cc112df10b265f5b
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-06-20 19:03:57 +0100

    update

--- pages/return.js
+++ pages/return.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import emailTemplate from '../templates/email';
+import emailTemplate from './templates/email';
 import Image from 'next/image';
 
 export default function Return() {
