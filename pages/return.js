c0153f6a44b98d7aa5b693c18e394b2f25220a0c
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-06-21 00:11:17 +0100

    changes

--- pages/return.js
+++ pages/return.js
@@ -66,20 +66,20 @@ export default function Return() {
 
   if (status === 'complete') {
     return (
-      <section id="success" style={{width: '475px'}}>
-        <Image src='/ugcmixtape.svg' alt='logo' width={300} height={50} />
-        <Image src='/success.svg' alt='success' width={300} height={300} style={{marginTop: '3rem'}} />
-        <h1 style={{fontSize: '2rem', fontWeight: '700', marginBottom: '0.5rem'}}>Payment Successful</h1>
-        <p style={{fontSize: '1.2rem', marginTop: '0', maxWidth: '100%', textAlign: 'center'}}>
-          Your payment was successful and a receipt has been sent to {customerEmail}.
-          Your order is on its way...
-        </p>
-        <a href='https://www.ugcmixtape.com/login'>
-          <button style={{padding: '1.2rem', borderRadius: '4px', color: 'white', backgroundColor: '#000000', border: 'none', fontSize: '1.2rem'}}>
-            Back to Login
-          </button>
-        </a>
-      </section>
+      <section id="success" style={styles.container}>
+          <Image src='/ugcmixtape.svg' alt='logo' width={300} height={150} />
+          <Image src='/success.svg' alt='success' width={300} height={300} style={{marginTop: '1rem'}} />
+      <h1 style={styles.heading}>Payment Successful</h1>
+      <p style={styles.paragraph}>
+        Your payment was successful and a receipt has been sent to {customerEmail}.
+        Your order is on its way...
+      </p>
+      <a href='https://www.ugcmixtape.com/login'>
+        <button style={styles.button}>
+          Back to Login
+        </button>
+      </a>
+    </section>
     );
   }
 
@@ -105,3 +105,41 @@ export default function Return() {
 
   return null;
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '320px',
+    maxWidth: '475px',
+    height: '60vh',
+    margin: '0 auto',
+    padding: '0',
+    boxSizing: 'border-box',
+    textAlign: 'center',
+  },
+  heading: {
+    fontSize: '1.6rem',
+    fontWeight: '700',
+    marginBottom: '0.5rem',
+  },
+  paragraph: {
+    fontSize: '1rem',
+    marginTop: '0',
+    maxWidth: '95%',
+    textAlign: 'center',
+    height: 'fit-content'
+  },
+  button: {
+    padding: '1.2rem',
+    borderRadius: '4px',
+    color: 'white',
+    backgroundColor: '#000000',
+    border: 'none',
+    fontSize: '1.2rem',
+    marginTop: '1rem',
+    cursor: 'pointer',
+  }
+};
