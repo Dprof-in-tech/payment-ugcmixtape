bb595b4eb47e88ddb2ea20ced18663368bb40956
Isaac Onyemaechi Ugwu <Amaechiisaac450@gmail.com>
2024-08-24 07:51:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,3 @@
-// import React, { useCallback } from 'react';
-// import { loadStripe } from '@stripe/stripe-js';
-// import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js';
-
-// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-
-// export default function App() {
-//   const fetchClientSecret = useCallback(async () => {
-//     const urlParams = new URLSearchParams(window.location.search);
-
-//     // Extract values from URL parameters (handle missing values gracefully)
-//     const name = urlParams.get('n') || '';
-//     const email = urlParams.get('e') || '';
-//     const id = urlParams.get('i') || '';
-//     const client = urlParams.get('cn') || '';
-//     const amount = parseFloat(urlParams.get('p')) || 0; // Default to 0 if not provided
-
-//     try {
-//       const response = await fetch("/api/checkout_sessions", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ name, email, amount, id, client }),
-//       });
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error ${response.status}`);
-//       }
-
-//       const data = await response.json();
-
-//       if (!data.clientSecret) {
-//         throw new Error("No client secret found in the response");
-//       }
-
-//       return data.clientSecret;
-//     } catch (error) {
-//       console.error("Error fetching client secret:", error);
-//       throw error;
-//     }
-//   }, []);
-
-//   const options = { fetchClientSecret };
-
-//   return (
-//     <div id="checkout">
-//       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-//         <EmbeddedCheckout />
-//       </EmbeddedCheckoutProvider>
-//     </div>
-//   );
-// }
-
-
 import React, { useState, useCallback } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js';
@@ -74,7 +19,7 @@ export default function App() {
     let amount = parseFloat(urlParams.get('p')) || 0;
 
     if (couponCode === 'SOSHE2024') {
-      amount -= Math.ceil(amount * 0.375);
+      amount -= Math.ceil(amount * 0.3324);
     }
 
     try {
